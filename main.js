7ee205a8f6485926260ecdb24901e196e8eff141
Tim Biddington <timbiddington@fastmail.com.au>
2022-01-03 12:44:35 +1100

    Move WebGL functions to webgl.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /**
  * Tatooine – Heavily commented
  */
-
+import { compile, initBuffer } from './webgl.js';
 import cube from './mesh/cube.js';
 import * as m from './matrix.js';
 
@@ -53,7 +53,7 @@ function main() {
   let program = {
     // User space
     state: {
-      position: [ 0,0, -155 ],
+      position: [ 0, 0, -155 ],
       matrices: {
         view: null
       },
@@ -76,7 +76,6 @@ function main() {
   }
 
   // Setup matrices
-  // Initial draw
   setup(gl, program);
 
   // Start app loop
@@ -84,102 +83,10 @@ function main() {
 }
 
 /**
- * 
- * @param {*} gl - The WebGL context
- * @param {*} vsSource Textual source of vertex shader
- * @param {*} fsSource Textual source of frament shader
- * @returns void
- * 
- * Compiles shaders and attaches them to a shader program
- */
-function compile(gl, vsSource, fsSource) {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-  let program = gl.createProgram();
-
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-
-  gl.bindAttribLocation(program, 0, 'a_position');
-
-  gl.linkProgram(program);
-
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    alert('Unable to initialize the shader program: ', + gl.getProgramInfoLog(program));
-    return null;
-  }
-
-  return program;
-}
-
-/**
- * 
- * @param {*} gl - The WebGL context
- * @param {*} type - gl.FRAGMENT_SHADER / gl.VERTEX_SHADER
- * @param {*} source - Textual source of shader
- * @returns - Compiled WebGL shader
- */
-function loadShader(gl, type, source) {
-  const shader = gl.createShader(type);
-
-  gl.shaderSource(shader, source);
-
-  gl.compileShader(shader);
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert('An error occured compiling the shader: ' + gl.getShaderInfoLog(shader));
-    return null;
-  }
-
-  return shader;
-}
-
-/**
- * Loading mesh data should take an array of numbers
- * representing our vertices (x,y,z) and load them
- * into buffers dynamically
- * @param {*} gl - WebGL Context
- * @returns - Object containing buffer objects
- */
-function initBuffer(gl, data = null) {
-
-  /*
-  *  bufferData(a,d)    bindBuffer(a,b)
-  *      \|/                 \|/
-  *       |                   |
-  *     --|-------------------|--
-  *    |  -›  ARRAY_BUFFER   ›-  |
-  *    |           |             |
-  *    |          /|\            |
-  *    | (bindBuffer,bufferData) |
-  *     -------------------------
-  */
-
-  // Create our gl buffer to fill with model vertices
-  const buffer = gl.createBuffer();
-
-  /* WebGL lets us manipulate many WebGL resources on global bind points - here gl.ARRAY_BUFFER.
-     You can think of bind points as internal global variables inside WebGL.
-     First you bind a resource to a bind point. Then, subsequent functions
-     refer to the resource through the bind point.
-
-     Here we bind the `positionBuffer` to ARRAY_BUFFER bind point
-  */
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-  // Notice we buffer data via our bind point (ARRAY_BUFFER) not the `positionBuffer` directly
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
-
-  return buffer;
-}
-
-/**
  * Static setup and initial draw - details that don't change after creation
  * or at the arrival of events
- * @param {*} gl 
- * @param {*} programInfo 
- * @param {*} buffers - object containing named buffers for a mesh e.g { position: GL.buffer }
+ * @param {*} gl
+ * @param {*} program 
  */
 function setup(gl, program) {
 
@@ -346,14 +253,15 @@ function start(gl, program) {
   // }
 
   function loop(){
+    // Set aside our frame id in case we need to cancel
     let frameId = requestAnimationFrame(loop);
-
+    // Perform one loop of the render
     {
       // Clear the buffer with the specified colour;
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      
+      // Select the mesh to render
       gl.bindVertexArray(program.state.vao);
-
+      // 
       const offset = 0;
       const vertexCount = 18; // 8 pieces of data in our buffer; 2 per vertex, hence 4
       gl.drawArrays(gl.TRIANGLES, offset, vertexCount);
---
+++ webgl.js
@@ -0,0 +1,97 @@
+
+/**
+ * 
+ * @param {*} gl - The WebGL context
+ * @param {*} vsSource Textual source of vertex shader
+ * @param {*} fsSource Textual source of frament shader
+ * @returns void
+ * 
+ * Compiles shaders and attaches them to a shader program
+ */
+ function compile(gl, vsSource, fsSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  let program = gl.createProgram();
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+
+  gl.bindAttribLocation(program, 0, 'a_position');
+
+  gl.linkProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    alert('Unable to initialize the shader program: ', + gl.getProgramInfoLog(program));
+    return null;
+  }
+
+  return program;
+}
+
+/**
+ * 
+ * @param {*} gl - The WebGL context
+ * @param {*} type - gl.FRAGMENT_SHADER / gl.VERTEX_SHADER
+ * @param {*} source - Textual source of shader
+ * @returns - Compiled WebGL shader
+ */
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  gl.shaderSource(shader, source);
+
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occured compiling the shader: ' + gl.getShaderInfoLog(shader));
+    return null;
+  }
+
+  return shader;
+}
+
+/**
+ * Loading mesh data should take an array of numbers
+ * representing our vertices (x,y,z) and load them
+ * into buffers dynamically
+ * @param {*} gl - WebGL Context
+ * @returns - Object containing buffer objects
+ */
+ function initBuffer(gl, data = null) {
+
+  /*
+  *  bufferData(a,d)    bindBuffer(a,b)
+  *      \|/                 \|/
+  *       |                   |
+  *     --|-------------------|--
+  *    |  -›  ARRAY_BUFFER   ›-  |
+  *    |           |             |
+  *    |          /|\            |
+  *    | (bindBuffer,bufferData) |
+  *     -------------------------
+  */
+
+  // Create our gl buffer to fill with model vertices
+  const buffer = gl.createBuffer();
+
+  /* WebGL lets us manipulate many WebGL resources on global bind points - here gl.ARRAY_BUFFER.
+     You can think of bind points as internal global variables inside WebGL.
+     First you bind a resource to a bind point. Then, subsequent functions
+     refer to the resource through the bind point.
+
+     Here we bind the `positionBuffer` to ARRAY_BUFFER bind point
+  */
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+  // Notice we buffer data via our bind point (ARRAY_BUFFER) not the `positionBuffer` directly
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+
+  return buffer;
+}
+
+export {
+  compile,
+  loadShader,
+  initBuffer
+}
